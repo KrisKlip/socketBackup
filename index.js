6bd4339e7271ae3f8d36aa9e9f47b87aeaffc564
KrisKlip <knklipfel@gmail.com>
2017-11-29 01:15:38 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 4000;
+var other_server = require("socket.io-client")('http://localhost:3000');
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'FinalSat')));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  console.log("connected")
+  other_server.emit('add user', "testes")
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // if (addedUser) return;
+    console.log(username)
+    // other_server.emit('add user', username);
+    // we store the username in the socket session for this client
+    // socket.username = username;
+    // ++numUsers;
+    // addedUser = true;
+    // socket.emit('login', {
+    //   numUsers: numUsers
+    // });
+    // // echo globally (all clients) that a person has connected
+    // socket.broadcast.emit('user joined', {
+    //   username: socket.username,
+    //   numUsers: numUsers
+    // });
+  });
+
+  socket.on('move', function (data) {
+    console.log(data);
+  });
+
+  // other_server.on('cursor display', function(data){
+  //   console.log(data);
+  // });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('cursor display', function (data) {
+    console.log(data);
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    
+  });
+});
